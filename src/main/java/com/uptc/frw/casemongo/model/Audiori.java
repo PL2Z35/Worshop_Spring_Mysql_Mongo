556c585c8ef5e2ea480624f9757d4e281187e59c
santi <santiago.rodriguez02@uptc.edu.co>
2024-05-14 18:51:34 -0500

    create a new class for auditory table in database

---
+++ src/main/java/com/uptc/frw/casemongo/model/Audiori.java
@@ -0,0 +1,30 @@
+package com.uptc.frw.casemongo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Audiori {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idAuditory;
+    private String nameTable;
+    private String oldRegister;
+    private String newRegister;
+    private String operation;
+    private Date date;
+
+}
