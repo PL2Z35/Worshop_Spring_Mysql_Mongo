7f3eb7a13912bbfdc1eb0e2e74f0fde9abedddd2
Willin98 <67207973+Willin98@users.noreply.github.com>
2024-05-13 19:01:47 -0500

    Project Initialization

---
+++ src/main/java/com/uptc/frw/casemongo/CaseMongoApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.casemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frw/casemongo/CaseMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.casemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
